9e35da6ef8ce511879beacb6c048a30f1c981f90
DashaTsybulkina <cybulkinadara@gmail.com>
2022-05-22 11:05:56 +0300

    Добавьте файлы проекта.

---
+++ TI_lab4/Form1.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace TI_lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_q = new System.Windows.Forms.TextBox();
+            this.textBox_k = new System.Windows.Forms.TextBox();
+            this.textBox_x = new System.Windows.Forms.TextBox();
+            this.textBox_h = new System.Windows.Forms.TextBox();
+            this.textBox_p = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.buttonSign = new System.Windows.Forms.Button();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxHash = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox_h0 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox_r = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox_s = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_q
+            // 
+            this.textBox_q.Location = new System.Drawing.Point(29, 42);
+            this.textBox_q.Name = "textBox_q";
+            this.textBox_q.Size = new System.Drawing.Size(100, 22);
+            this.textBox_q.TabIndex = 0;
+            // 
+            // textBox_k
+            // 
+            this.textBox_k.Location = new System.Drawing.Point(32, 218);
+            this.textBox_k.Name = "textBox_k";
+            this.textBox_k.Size = new System.Drawing.Size(100, 22);
+            this.textBox_k.TabIndex = 1;
+            // 
+            // textBox_x
+            // 
+            this.textBox_x.Location = new System.Drawing.Point(32, 174);
+            this.textBox_x.Name = "textBox_x";
+            this.textBox_x.Size = new System.Drawing.Size(100, 22);
+            this.textBox_x.TabIndex = 2;
+            // 
+            // textBox_h
+            // 
+            this.textBox_h.Location = new System.Drawing.Point(29, 130);
+            this.textBox_h.Name = "textBox_h";
+            this.textBox_h.Size = new System.Drawing.Size(100, 22);
+            this.textBox_h.TabIndex = 3;
+            // 
+            // textBox_p
+            // 
+            this.textBox_p.Location = new System.Drawing.Point(29, 86);
+            this.textBox_p.Name = "textBox_p";
+            this.textBox_p.Size = new System.Drawing.Size(100, 22);
+            this.textBox_p.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите q:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите k:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Введите x:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Введите h:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Введите p:";
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(32, 322);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(100, 42);
+            this.buttonOpenFile.TabIndex = 10;
+            this.buttonOpenFile.Text = "Открыть файл";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // buttonSign
+            // 
+            this.buttonSign.Location = new System.Drawing.Point(32, 370);
+            this.buttonSign.Name = "buttonSign";
+            this.buttonSign.Size = new System.Drawing.Size(100, 33);
+            this.buttonSign.TabIndex = 11;
+            this.buttonSign.Text = "Подписать";
+            this.buttonSign.UseVisualStyleBackColor = true;
+            this.buttonSign.Click += new System.EventHandler(this.buttonSign_Click);
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(32, 410);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(100, 30);
+            this.buttonCheck.TabIndex = 12;
+            this.buttonCheck.Text = "Проверить";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(233, 42);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ReadOnly = true;
+            this.textBoxInput.Size = new System.Drawing.Size(184, 248);
+            this.textBoxInput.TabIndex = 13;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(454, 42);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.Size = new System.Drawing.Size(184, 248);
+            this.textBoxOutput.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(230, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 16);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Исходный текст";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(451, 23);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 16);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Результат";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxHash
+            // 
+            this.textBoxHash.Location = new System.Drawing.Point(233, 322);
+            this.textBoxHash.Multiline = true;
+            this.textBoxHash.Name = "textBoxHash";
+            this.textBoxHash.ReadOnly = true;
+            this.textBoxHash.Size = new System.Drawing.Size(181, 61);
+            this.textBoxHash.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(230, 303);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 16);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Хэш:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(29, 243);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 16);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Введите H0:";
+            // 
+            // textBox_h0
+            // 
+            this.textBox_h0.Location = new System.Drawing.Point(32, 262);
+            this.textBox_h0.Name = "textBox_h0";
+            this.textBox_h0.Size = new System.Drawing.Size(100, 22);
+            this.textBox_h0.TabIndex = 20;
+            this.textBox_h0.Text = "100";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(230, 387);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(20, 16);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "R:";
+            // 
+            // textBox_r
+            // 
+            this.textBox_r.Location = new System.Drawing.Point(233, 406);
+            this.textBox_r.Name = "textBox_r";
+            this.textBox_r.ReadOnly = true;
+            this.textBox_r.Size = new System.Drawing.Size(100, 22);
+            this.textBox_r.TabIndex = 22;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(230, 431);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(19, 16);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "S:";
+            // 
+            // textBox_s
+            // 
+            this.textBox_s.Location = new System.Drawing.Point(233, 450);
+            this.textBox_s.Name = "textBox_s";
+            this.textBox_s.ReadOnly = true;
+            this.textBox_s.Size = new System.Drawing.Size(100, 22);
+            this.textBox_s.TabIndex = 24;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(483, 322);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 26;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(483, 361);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 27;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(483, 410);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 28;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(483, 450);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 29;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 484);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox_s);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox_r);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox_h0);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxHash);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.buttonSign);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_p);
+            this.Controls.Add(this.textBox_h);
+            this.Controls.Add(this.textBox_x);
+            this.Controls.Add(this.textBox_k);
+            this.Controls.Add(this.textBox_q);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_q;
+        private System.Windows.Forms.TextBox textBox_k;
+        private System.Windows.Forms.TextBox textBox_x;
+        private System.Windows.Forms.TextBox textBox_h;
+        private System.Windows.Forms.TextBox textBox_p;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Button buttonSign;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxHash;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox_h0;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox_r;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox_s;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ TI_lab4/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TI_lab4
+{
+    public partial class Form1 : Form
+    {
+        string fileName;
+        byte[] fileText;
+        string fileTextStr;
+        string extension;
+        string outputStr;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            fileName = openFileDialog1.FileName;
+            textBoxOutput.Text = "";
+            textBoxInput.Text = "";
+            textBoxHash.Text = "";
+            textBox_r.Text = "";
+            textBox_s.Text = "";
+            fileText = File.ReadAllBytes(fileName);
+            fileTextStr = File.ReadAllText(fileName);
+            textBoxInput.Text = fileTextStr;
+            extension = Path.GetExtension(fileName);
+        }
+        private static BigInteger Pow(BigInteger value, BigInteger power, BigInteger mod)
+        {
+            BigInteger a1 = value;
+            BigInteger z1 = power;
+            BigInteger x = 1;
+            while (z1 != 0)
+            {
+                while ((z1 % 2) == 0)
+                {
+                    z1 /= 2;
+                    a1 = (a1 * a1) % mod;
+                }
+
+                z1--;
+                x = (x * a1) % mod;
+            }
+
+            return x;
+        }
+
+        private BigInteger GetHash(byte[] data, BigInteger q,BigInteger p)
+        {
+            BigInteger hash = BigInteger.Parse(textBox_h0.Text);
+            foreach (var b in data)
+            {
+                hash = Pow(hash + b, 2, q);
+            }
+            textBoxHash.Text = hash.ToString();
+            return hash;
+        }
+
+        public bool MillerRabinTest(BigInteger n, int k)
+        {
+            if (n == 2 || n == 3)
+                return true;
+            if (n < 2 || n % 2 == 0)
+                return false;
+            BigInteger t = n - 1;
+            int s = 0;
+            while (t % 2 == 0)
+            {
+                t /= 2;
+                s += 1;
+            }
+            for (int i = 0; i < k; i++)
+            {
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+                byte[] _a = new byte[n.ToByteArray().LongLength];
+                BigInteger a;
+                do
+                {
+                    rng.GetBytes(_a);
+                    a = new BigInteger(_a);
+                }
+                while (a < 2 || a >= n - 2);
+                BigInteger x = BigInteger.ModPow(a, t, n);
+                if (x == 1 || x == n - 1)
+                    continue;
+                for (int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, n);
+                    if (x == 1)
+                        return false;
+                    if (x == n - 1)
+                        break;
+                }
+                if (x != n - 1)
+                    return false;
+            }
+            return true;
+        }
+        private void buttonSign_Click(object sender, EventArgs e)
+        {
+            if (textBox_q.Text != ""&& textBox_p.Text != ""&& textBox_h.Text != ""&& textBox_x.Text != "" && textBox_k.Text != "") {
+                BigInteger q = BigInteger.Parse(textBox_q.Text);
+                BigInteger p = BigInteger.Parse(textBox_p.Text);
+                BigInteger h = BigInteger.Parse(textBox_h.Text);
+                BigInteger x = BigInteger.Parse(textBox_x.Text);
+                BigInteger k = BigInteger.Parse(textBox_k.Text);
+                BigInteger g = Pow(h, (p - 1) / q, p);
+                if (!MillerRabinTest(p, 100) || !MillerRabinTest(q, 100) || ((p - 1) % q != 0) || (h <= 1 || h >= p - 1) || (g <= 1) || (x <= 0 || x >= q) || (k <= 0 && k >= q))
+                {
+                    MessageBox.Show("Неверные данные!");
+                }
+                else
+                {
+                    BigInteger hash = GetHash(fileText, q, p);
+                    BigInteger r = Pow(g, k, p) % q;
+                    textBox_r.Text = r.ToString();
+                    BigInteger s = ((hash + x * r) * Pow(k, q - 2, q)) % q;
+                    textBox_s.Text = s.ToString();
+                    if (r == 0 || s == 0)
+                    {
+                        MessageBox.Show("Неверные данные!");
+                    }
+                    else
+                    {
+                        textBox_r.Text = r.ToString();
+                        textBox_s.Text = s.ToString();
+                        outputStr = fileTextStr + ',' + r.ToString() + ',' + s.ToString();
+                        textBoxOutput.Text = outputStr;
+                        int temp = fileName.IndexOf(extension);
+                        string pathout = fileName.Remove(temp, extension.Length);
+                        pathout = pathout + "Output" + extension;
+                        File.WriteAllText(pathout, outputStr);
+                    }
+                }
+            }
+        }
+
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            if (textBox_q.Text != "" && textBox_p.Text != "" && textBox_h.Text != "" && textBox_x.Text != "" && textBox_k.Text != "")
+            {
+                BigInteger q = BigInteger.Parse(textBox_q.Text);
+                BigInteger p = BigInteger.Parse(textBox_p.Text);
+                BigInteger x = BigInteger.Parse(textBox_x.Text);
+                BigInteger h = BigInteger.Parse(textBox_h.Text);
+                BigInteger g = Pow(h, (p - 1) / q, p);
+                if (!MillerRabinTest(p, 100) && !MillerRabinTest(q, 100) && ((p - 1) % q != 0) && (h <= 1 || h >= p - 1) && (g <= 1))
+                {
+                    MessageBox.Show("Неверные данные!");
+                }
+                else
+                {
+                    string[] textStr = fileTextStr.Split(',');
+                    fileText = Encoding.UTF8.GetBytes(textStr[0]);
+                    BigInteger r = BigInteger.Parse(textStr[1]);
+                    BigInteger s = BigInteger.Parse(textStr[2]);
+                    BigInteger y = Pow(g, x, p);
+                    BigInteger hash = GetHash(fileText, q, p);
+                    BigInteger w = Pow(s, q - 2, q);
+                    BigInteger u1 = (hash * w) % q;
+                    BigInteger u2 = (r * w) % q;
+                    textBox3.Text = r.ToString();
+                    BigInteger v = ((Pow(g, u1, p) * Pow(y, u2, p)) % p) % q;
+                    textBox4.Text = v.ToString();
+                    if (v == r)
+                    {
+                        textBoxOutput.Text = "Подпись подлинна";
+                    }
+                    else
+                    {
+                        textBoxOutput.Text = "Подпись некоректна";
+                    }
+                }
+            }
+        }
+    }
+}
